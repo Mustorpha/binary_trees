ebb7ae153ab8bc0f7a93f8bdf4fc0fe13b681f10
MrVeeJon <victor.jonathan.u@gmail.com>
2024-01-04 19:52:11 -0800

    To delete an entire binanry tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_delete - Deletes a binary tree.
- * @tree: A pointer to the root node of the tree to delete.
+ * @tree: Points to the root node of the tree to delete.
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
