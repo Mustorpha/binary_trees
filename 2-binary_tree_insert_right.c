83eea2aeea7fef8cc703bf68bd58d7b239c7c065
MrVeeJon <victor.jonathan.u@gmail.com>
2024-01-04 19:49:47 -0800

    Modified right insertion to binanry trees

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_insert_right - Insert a node as the right-child
  *                            of another in a binary tree.
- * @parent: A pointer to the node to insert the right-child in.
+ * @parent: Points to the node to insert the right-child in.
  * @value: The value to store in the new node.
  *
  * Return: If parent is NULL or an error occurs - NULL.
