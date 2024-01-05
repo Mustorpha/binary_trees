91a21f2ace8f383dad8020c1e53f7e4e3997088c
MrVeeJon <victor.jonathan.u@gmail.com>
2024-01-04 19:54:30 -0800

    Checking if a node is a leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_is_leaf - Checks if a node is a leaf of a binary tree.
- * @node: A pointer to the node to check.
+ * @node: Points to the node to check.
  *
  * Return: If the node is a leaf - 1.
  *         Otherwise - 0.
